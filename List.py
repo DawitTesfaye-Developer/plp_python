54ad24a723ed5042f5f06454002cb33c295017a6
Dawit Tesfaye <dawit@dawit.dawit>
2025-08-01 18:53:16 -0400

    i have added two files

--- List.py
+++ List.py
@@ -11,11 +11,14 @@ my_list.insert(1,15)
 # Extending the list with multiple elements
 my_list.extend([50,60,70])
 # Removing the last element from the list
-my_list.pop()
 # sorting the list in ascending order
-my_list.sort()
-
+#my_list.insert(len(my_list),100)
+my_list.insert(7,100)
+my_list.extend([102,200,300])
+del my_list[2]
 # Printing the list and the index of the element 30
 
-print("print the list :" , my_list)
-print("the index of 30 is : " , my_list.index(30))
+# for item in my_list:
+#     print(item)
+print(my_list)
+print(my_list[-3])
---
+++ python_Week2_assignment.py
@@ -0,0 +1,13 @@
+#python week two assignment
+
+#1.Create an empty list called my_list.
+mylist = []
+
+#Append the following elements to my_list: 10, 20, 30, 40.
+mylist.append(10)
+mylist.append(20)
+mylist.append(30)
+mylist.append(40)
+
+#
+mylist.insert(2,15)
