dd1efd3a756e59030a59e06d0f1b2bf2f5cd05dd
Dawit Tesfaye <dawit@dawit.dawit>
2025-08-01 19:11:41 -0400

    i have added Extend my_list with another list: [50, 60, 70] on python week2_assignment.py and different anothers

--- python_Week2_assignment.py
+++ python_Week2_assignment.py
@@ -9,5 +9,23 @@ mylist.append(20)
 mylist.append(30)
 mylist.append(40)
 
-#
+print(mylist)
+#Insert the value 15 at the second position in the list.
 mylist.insert(2,15)
+print(mylist)
+
+#Extend my_list with another list: [50, 60, 70].
+mylist.extend([50,60,70])
+print(mylist)
+
+#Remove the last element from my_list.
+mylist.pop()
+print(mylist)
+
+# sort mulist in ascending order
+mylist.sort()
+print(mylist)
+
+#find and print the index of value 30 in mylist
+print(mylist.index(30))
+
